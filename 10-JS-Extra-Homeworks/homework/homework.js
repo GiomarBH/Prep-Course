56f6143835901f3fb721b8fa73b7878adf475513
unknown <gbolivarh@gmail.com>
2022-05-03 02:21:55 -0500

    Homework JS10 listo!.

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,12 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+var ArrayN = []
+
+for(var clave in objeto){
+  ArrayN.push([clave, objeto[clave]]);
+}
+return ArrayN;
 }
 
 
@@ -18,6 +24,15 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+var objeto ={};
+for( var i=0; i<string.length; i++ ){
+  if(objeto.hasOwnProperty(string[i])){
+    objeto[string[i]]= objeto[string[i]]+1;
+  }else{
+    objeto[string[i]] = 1;
+  }
+}
+return objeto;
 }
 
 
@@ -26,6 +41,17 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var mayus ='';
+  var minus ='';
+for( var i=0; i<s.length; i++ ){
+  if(s[i] == s[i].toLowerCase()){
+    minus += s[i];
+  }else{
+    mayus += s[i];
+  }
+}
+return mayus + minus ;
+
 }
 
 
@@ -35,6 +61,20 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+
+//  var tmp = str.length;
+ // var espejo ='';
+//for( var i=0; i<str.length; i++ ){ 
+//  espejo[tmp]=str[i]
+  //tmp = tmp -1 ;
+//}
+//return espejo ;
+
+var array = str.split(' ')
+var arrayf = array.map(function(ele){
+  return ele.split('').reverse().join('');
+})
+return arrayf.join(' ');
 } 
 
 
@@ -43,6 +83,13 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var str = numero.toString();
+  var result = str.split('').reverse().join('');
+  if (str === result){
+    return 'Es capicua'
+  }else{
+    return  'No es capicua'
+  }
 }
 
 
@@ -50,6 +97,16 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+
+  var result ='';
+for( var i=0; i<cadena.length; i++ ){
+  if(cadena[i] === 'a' || cadena[i] === 'b' || cadena[i] === 'c'){
+    continue;
+  }else{
+    result += cadena[i];
+  }
+}
+return result ;
 }
 
 
@@ -57,17 +114,47 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
-}
+  var result ='';
 
+
+  //sortArray(arr);
+  //arrays.sortArray(arr);
+  //for (var i=0;i<arr.length;i++){
+
+    var nuevoArray = arr.sort(function (a, b) {
+      return a.length - b.length
+    })
+    return nuevoArray;
+  //}
+}
 
 function buscoInterseccion(arreglo1, arreglo2){
   //Existen dos arrays, cada uno con 5 números. A partir de ello, escribir una función que permita 
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
+var union =[];
+//var ind = 0;
+for(var i=0 ; arreglo1.length ;i++){
+  for(var j=0 ; arreglo2.length ;j++){
+    //union[ind] += arreglo1[i];
+    if (arreglo1[i] == arreglo2[j]){
+      union.push(arreglo1[i])
+    
+    //  for(var k=0;union.length ;j++){
+      //  if (arreglo1[i] === union[k]){
+        //  continue;
+          //}else{
+          //  union[ind] += arreglo1[i];
+          //}
+      //}
+    }
+    //break;
+  }
 }
+return union;
 
+}
 
 
 // No modificar nada debajo de esta línea
